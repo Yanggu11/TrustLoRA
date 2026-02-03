3caaba45be89dc08f39624e8e0d836a68e746640
btrojan-official <bartosztrojanofficial@gmail.com>
2026-02-03 12:27:05 +0100

    fix: sstb config  - dataset name fixed

--- params/lora_baselines/sts_b.py
+++ params/lora_baselines/sts_b.py
@@ -1,6 +1,6 @@
 params = {
     # most important, general params
-    "glue_dataset_name": "sst_b",
+    "glue_dataset_name": "sstb",
     "model_name": "roberta-base",
     "peft_model_name": "./pretrained_models/mnli_r_base_mnli_1769686375/checkpoint-736320",
     "use_hypernet": False,
